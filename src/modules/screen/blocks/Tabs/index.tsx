89c3ed6b6edf697370ebe213e53d919d8d1b0b77
edelgarat <edelgarat@yandex.ru>
2020-09-28 14:23:02 +0300

    Значение текущего таба из url

--- src/modules/screen/blocks/Tabs/index.tsx
+++ src/modules/screen/blocks/Tabs/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useHistory, useLocation } from "react-router";
+import qs from "querystring";
 
 import Tabs from "primitives/Tabs";
 import Wrapper from "primitives/Wrapper";
@@ -11,10 +13,33 @@ import BlockRenderer from "modules/screen/BlockRenderer";
 
 import { BlockInterface } from "state/globalState";
 
-function TabsBlock({ options }: BlockInterface<{ title: string; block: BlockInterface }[]>) {
+function TabsBlock({ options }: BlockInterface<{ key?: string; tabs: { title: string; block: BlockInterface }[] }>) {
+  const history = useHistory();
+  const location = useLocation();
+
+  const key = options!.key || "tab";
+
+  function getParsedQuery() {
+    return qs.parse(location.search.slice(1));
+  }
+
+  const activeIndex = React.useMemo(() => {
+    const parsedQuery = getParsedQuery();
+    if (!parsedQuery[key]) return 0;
+    const value = parseFloat(parsedQuery[key] as string);
+    if (isNaN(value)) return 0;
+    if (value >= options!.tabs.length) return 0;
+    return value;
+  }, [location]);
+
+  function setActiveIndex(index: number) {
+    const parsedQuery = getParsedQuery();
+    history.push({ search: qs.encode({ ...parsedQuery, [key]: index.toString() }) });
+  }
+
   const tabs = React.useMemo(
     () =>
-      options!.map((tab) => ({
+      options!.tabs.map((tab) => ({
         render: () => (
           <LoadingProvider>
             {(loadingProviderRef) => (
@@ -32,7 +57,7 @@ function TabsBlock({ options }: BlockInterface<{ title: string; block: BlockInte
     [],
   );
 
-  return <Tabs items={tabs} />;
+  return <Tabs items={tabs} activeIndex={activeIndex} setActiveIndex={setActiveIndex} />;
 }
 
 export default React.memo(TabsBlock);
