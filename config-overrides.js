3d42d8ae1faf70991a1d292c3d145590b7f5d498
edelgarat <edelgarat@yandex.ru>
2021-02-09 17:15:54 +0300

    Добавлен прогресс плагин

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,11 @@
 const rewireSvgLoader = require("react-app-rewire-external-svg-loader");
 const rewireStyledComponents = require("react-app-rewire-styled-components");
+const ProgressPlugin = require("webpack").ProgressPlugin;
 
 module.exports = function override(config, env) {
   config = rewireStyledComponents(config, env);
   config = rewireSvgLoader(config, env);
+  config.plugins.push(new ProgressPlugin());
 
   return config;
 };
