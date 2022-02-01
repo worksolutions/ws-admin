5ee90f48c3213348024da90140130c27b81d85d8
belikcost <k.belik@worksolutions.ru>
2022-02-01 10:27:41 +0300

    Пофикшена ошибка с несохраняющимися статьями с Image stack

--- proxyServer/src/main.ts
+++ proxyServer/src/main.ts
@@ -3,6 +3,7 @@ import { join } from "path";
 import { NestFactory } from "@nestjs/core";
 
 import { config } from "dotenv";
+import { json, urlencoded } from "express";
 
 declare const module: any;
 
@@ -15,6 +16,10 @@ if (!process.env.NODE_PROXY_PORT) {
 async function bootstrap() {
   const { AppModule } = await import("./app.module");
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
   await app.listen(process.env.NODE_PROXY_PORT);
 
   if (module.hot) {
